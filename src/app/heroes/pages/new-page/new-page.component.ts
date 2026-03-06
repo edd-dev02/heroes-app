c04092fdc17f79c605d2426f463d47eaa55274f3
Eduardo Sandoval <edsandoval.dev@gmail.com>
2026-03-05 21:50:27 -0600

    Se crean métodos HTTP principales en servicio, creación del formulario para agregar / editar heroe, implementación de ReactiveForms y funcionalidad para agregar un nuevo héroe

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-new-page',
@@ -6,5 +9,66 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class NewPageComponent {
+
+  // Nota: Cuando trabajamos con formularios reactivos, la mayor parte de la lógica ira en el archivo del componente y no en la plantilla
+
+  public heroForm = new FormGroup({
+    id: new FormControl(''),
+    superhero: new FormControl('', { nonNullable: true }), // No se toleran valores null para este campo
+    publisher: new FormControl<Publisher>(Publisher.DCComics), // Valor por defecto
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
+
+    // Si es un campo númerico: campo: new FormControl(0)
+  });
+
+  public publishers = [
+    {
+      id: "DC Comics",
+      value: "DC - Comics"
+    },
+    {
+      id: "Marcel Comics",
+      value: "Marvel - Comics"
+    },
+  ];
+
+  constructor( private herosService: HeroesService ) {}
+
+  get currentHero(): Hero {
+
+    const hero = this.heroForm.value as Hero;
+
+    return hero;
+
+  }
+
+  onSubmit(): void {
+
+    if(this.heroForm.invalid) return;
+
+    // Si tiene id, significa que quiere actualizar un heroe existente
+    if(this.currentHero.id) {
+      this.herosService.updateHero( this.currentHero )
+        .subscribe( hero => {
+
+          // TODO: Mostrar snackbar
+
+
+
+        } );
+
+        return;
+    }
+
+    // Si no tiene id, significa que quiere crear un héroe nuevo
+    this.herosService.addHero( this.currentHero )
+      .subscribe( hero => {
+        //TODO: Mostrar snackbar, y navegar a /heroes/edit/hero.id
+      } );
+
+  }
 
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, of, tap } from 'rxjs';
+import { catchError, map, Observable, of, tap } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from '../../../environments/environments';
 
@@ -30,6 +30,30 @@ export class HeroesService {
   getSuggestions(query: string): Observable<Hero[]> {
 
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=6`);
+
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+
+    return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero);
+
+  }
+
+  updateHero(hero: Hero): Observable<Hero> {
+
+    if(!hero.id) throw Error("Hero ID is required");
+
+    return this.http.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`, hero);
+
+  }
+
+  deleteHeroById(id: string): Observable<boolean> {
+
+    return this.http.delete<Hero>(`${this.baseUrl}/heroes/${id}`)
+      .pipe(
+        catchError( err => of(false)), // No se borró nada
+        map( resp => true ) // Si se borró el objeto, retornamos un true
+      )
 
   }
 
