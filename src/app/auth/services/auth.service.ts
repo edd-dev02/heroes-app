eb6aae3117baca4ab4a796e0da5d1722fc617da7
Eduardo Sandoval <edsandoval.dev@gmail.com>
2026-03-09 21:09:13 -0600

    Se implementa Guard para proteger las rutas principales si no esta una sesión activa

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environments } from '../../../environments/environments';
 import { User } from '../interfaces/user.interface';
-import { Observable, tap } from 'rxjs';
+import { Observable, of, tap, map, catchError } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
@@ -27,6 +27,22 @@ export class AuthService {
         tap(user => this.user = user),
         // tap(user => localStorage.setItem( "token", user.id.toString() )),
         tap(user => localStorage.setItem( "token", "cefvtwbg<r.wfgt4g5yh.ertg5g4twh5y")),
+      );
+
+  }
+
+  checkAuthentication(): Observable<boolean> {
+
+    //if(!localStorage.getItem("token")) return false;
+    if(!localStorage.getItem("token")) return of(false);
+
+    const token = localStorage.getItem("token");
+
+    return this.http.get<User>(`${this.baseUrl}/users/1`)
+      .pipe(
+        tap(user => this.user = user),
+        map(user => !!user),
+        catchError(err => of(false))
       );
 
   }
